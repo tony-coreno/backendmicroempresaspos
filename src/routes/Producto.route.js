05359ee7eba8f9550832eb932cab2c104a5a5778
tony-coreno <ja.coreno@edu.utc.mx>
2021-06-04 23:18:06 -0500

    modificacion route prod

--- src/routes/Producto.route.js
+++ src/routes/Producto.route.js
@@ -1,30 +1,29 @@
-const { Router } = require('express');
-const router = Router()
-const ProductoCtrl = require('../controllers/productos/Productos.controllers');
-const Auth = require('../helpers/Auth')
+const { Router } = require("express");
+const router = Router();
+const ProductoCtrl = require("../controllers/productos/Productos.controllers");
+const Auth = require("../helpers/Auth");
 //Ruta para agregar el producto (POST)
 
-router.post('/agregar',Auth.verificartoken, ProductoCtrl.agregarProducto);
+router.post("/agregar", Auth.verificartoken, ProductoCtrl.agregarProducto);
 
+router.get("/listarporadmin/:id", ProductoCtrl.productosAdmin);
 
-router.get('/listarporadmin/:id', ProductoCtrl.productosAdmin);
-
-router.get('/listarporadmin/:jefe', ProductoCtrl.productosAdminEmpleado);
+router.get("/listarporadmin/:jefe", ProductoCtrl.productosAdminEmpleado);
 
 //Enviar a carrito
-router.get('/carrito/:sku', ProductoCtrl.productoACarrito);
+router.get("/carrito/:sku", ProductoCtrl.productoACarrito);
 
 //Eliminar
 
-router.delete('/eliminar/:id',ProductoCtrl.eliminar)
+router.delete("/eliminar/:id", ProductoCtrl.eliminar);
 
 //Buscar producto por nombre y ID de admin correspondiente
 
-router.get('/buscar/:producto/:id',Auth.verificartoken, ProductoCtrl.buscarProducto);
+router.get("/buscar/:producto/:id", ProductoCtrl.buscarProducto);
 
 //Actualizar Proveedor
-router.put('/actualizar/:id', ProductoCtrl.actualizar);
+router.put("/actualizar/:id", ProductoCtrl.actualizar);
 
-router.get('/buscar/:id', ProductoCtrl.buscarProd);
+router.get("/buscar/:id", ProductoCtrl.buscarProd);
 
 module.exports = router;
